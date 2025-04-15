9f1e19e03d32ae4e66b2fd026a13db014c28559e
Moritz <moritz_haaf@outlook.com>
2025-04-16 00:17:00 +0200

    optimized menubar

--- script.js
+++ script.js
@@ -4,8 +4,16 @@ function toggleMenu() {
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
-  document.body.classList.toggle("menu-open");
 }
+document.addEventListener("click", function (e) {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+
+  if (!menu.contains(e.target) && !icon.contains(e.target)) {
+    menu.classList.remove("open");
+    icon.classList.remove("open");
+  }
+});
 
 // Toggle job details in experience section
 function toggleDetails(button) {
